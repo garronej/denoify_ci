4f1dbb1eecafe53521d87e18491d888a0f90f7aa
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-05-05 23:25:44 +0200

    update

--- src/test/myFunction.ts
+++ src/test/myFunction.ts
@@ -3,13 +3,14 @@ import { myFunction } from "..";
 
 import { getPromiseAssertionApi } from "evt/dist/tools/testing/getPromiseAssertionApi";
 
-const { mustResolve } = getPromiseAssertionApi();
+const { mustResolve } = getPromiseAssertionApi({ "takeIntoAccountArraysOrdering": true});
 
 (async () => {
 
     await mustResolve({
         "promise": myFunction(),
-        "expectedData": ["a", "b", "c"]
+        "expectedData": ["a", "b", "c"],
+        "delay": 0
     });
 
     console.log("PASS");
--- src/test/myObject.ts
+++ src/test/myObject.ts
@@ -11,5 +11,7 @@ assert(
     )
 );
 
+console.log("PASS");
+
 
 
